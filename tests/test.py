61e2a76ef648012f701ec263f4c18fb8932e7282
Hao Liu <hao.liu@afterpay.com>
2022-05-11 11:14:39 +0800

    updated output scripts

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 from streaming_feature_simulator.sql_generator import sql_generator
 
-test_input = '/Users/haoliu/github/Streaming_Feature_Simulator/streaming_feature_simulator/config/example_sliding_window_raw_address.json'
-sql = sql_generator(test_input, save_path='./output/raw_shipping_result.sql')
+test_input = '/Users/haoliu/github/Streaming_Feature_Simulator/streaming_feature_simulator/config/example_sliding_window_order_attempt.json'
+sql = sql_generator(test_input, save_path='./output/order_attempt_result.sql')
 print(sql)
 
